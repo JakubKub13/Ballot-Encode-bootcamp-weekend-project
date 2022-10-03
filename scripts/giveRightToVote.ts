b63d9dfe27b6392c933e65a1ea2456540ba1ae05
JK1331 <jakubkubala3@gmail.com>
2022-10-03 13:12:19 +0200

    Working on additional scripts

--- scripts/giveRightToVote.ts
+++ scripts/giveRightToVote.ts
@@ -1,7 +1,12 @@
-import { ethers } from "hardhat";
+import { ethers, getNamedAccounts } from "hardhat";
+import { Ballot } from "../typechain-types";
 
 
 async function main() {
+    let BallotGoerli: Ballot
+    const { deployer } = await getNamedAccounts();
+    BallotGoerli = await ethers.getContractAt("Ballot", "0x20CA848E5460D7564c482A2D24c97B85aaff5960", deployer)
+    console.log(await BallotGoerli.chairperson())
 
 }
 
