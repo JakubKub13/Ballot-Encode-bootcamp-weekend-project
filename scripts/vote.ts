79815b867a39be3cf43016e84ee1de6ff5ce3d0f
JK1331 <jakubkubala3@gmail.com>
2022-10-03 14:23:51 +0200

    Working on additional scripts

--- scripts/vote.ts
+++ scripts/vote.ts
@@ -9,9 +9,9 @@ async function vote() {
 
     Martin = await ethers.getSigner("0x0148756feD750C4Ba9232297a88B6b82690223ad")
 
-    //BallotGoerli = await ethers.getContractAt("Ballot", "0x20CA848E5460D7564c482A2D24c97B85aaff5960", Jacob)
-    //const txResponse = await BallotGoerli.delegate(Martin.address)
-    console.log(deployer)
+    BallotGoerli = await ethers.getContractAt("Ballot", "0x20CA848E5460D7564c482A2D24c97B85aaff5960", Martin)
+    const txResponse = await BallotGoerli.vote(0)
+    console.log(txResponse)
    
 }
 
